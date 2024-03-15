d6b36832f4d467310a704468655a1370086d996b
Fajar Khaswara <fkhaswara@gmail.com>
2024-03-15 16:23:52 +0700

    initial commit

---
+++ src/main/java/com/fajar/personalcontact/PersonalContactApplication.java
@@ -0,0 +1,13 @@
+package com.fajar.personalcontact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalContactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalContactApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fajar/personalcontact/PersonalContactApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fajar.personalcontact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalContactApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
